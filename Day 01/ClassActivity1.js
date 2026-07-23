8df55cf73fbc509ca640264c1e7fb4f594dc97cf
rupachem-debug <rupachem@gmail.com>
2026-07-24 00:26:17 +0530

    Add files via upload

---
+++ Day 01/ClassActivity1.js
@@ -0,0 +1,31 @@
+for(let i=0;i<10;i++){console.log('Rupa')}
+console.log('hello world')
+//1. String datatype
+var firstName
+firstName = 'RupaK'
+console.log(firstName)
+
+//2. Number datatype
+var empID
+empID = 8565117
+console.log(empID)
+
+//3. Boolean datatype
+var insured
+insured = false
+if (insured)
+{console.log(firstName + ' is insured')}
+else
+{console.log(firstName + ' is not insured')}
+
+
+
+//4. undefined datatype
+var mobileNumber
+console.log(mobileNumber)
+
+//5. null datatype
+var address = null
+console.log(address)
+
+console.log(typeof firstName, typeof empID, typeof insured, typeof mobileNumber, typeof address)
