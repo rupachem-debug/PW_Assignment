d1d2e12bc07bc9619a14d62595a1db25640ab598
rupachem-debug <rupachem@gmail.com>
2026-07-28 19:53:29 +0530

    Add files via upload

---
+++ Day 02/Assignment_Odd_or_Even.js
@@ -0,0 +1,13 @@
+let rollNo = 8
+function isOddOrEven(rollNo)
+{
+if (rollNo % 2 === 0)
+{
+    console.log("It is a Even number")
+}
+else
+{
+    console.log("It is a Odd number")
+}
+}
+isOddOrEven(rollNo)
